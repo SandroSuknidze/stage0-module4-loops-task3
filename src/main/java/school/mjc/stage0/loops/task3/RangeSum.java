a3be353ac7a1ff92a2b7cc1632bd7c3b7c24f5e6
Sandro Suqnidze <sandrosuqnidze2@gmail.com>
2023-09-11 10:51:20 +0400

    For loops (changed)

--- src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/RangeSum.java
@@ -2,6 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class RangeSum {
     public void printSumInclusive(int firstBoarder, int secondBoarder) {
-        System.out.println(firstBoarder + secondBoarder);
+        int k = 0;
+
+        int bigger = firstBoarder > secondBoarder ? firstBoarder : secondBoarder;
+        int smaller = firstBoarder < secondBoarder ? firstBoarder : secondBoarder;
+
+        for (int i = smaller; i <= bigger; i++) {
+            k += i;
+        }
+
+        System.out.println(k);
     }
 }
